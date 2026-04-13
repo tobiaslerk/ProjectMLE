d338bd0b5b1cb56e37e79dfc5aaa1c98ba9b6e94
Simen Borgerud Moe <simenny2506@gmail.com>
2026-04-13 10:35:08 +0200

    Changed the implementation of estimators, should be more correct in accordance to project specs, and more computationallly efficient

--- estimation.py
+++ estimation.py
@@ -14,43 +14,35 @@ def X(t, standard_deviation):
 
     return A * np.exp(1j * (w_0 * t + phi)) + complex_noise
 
-def W_estimate_FFT(x, M):
-    fft_x = np.fft.fft(x, M)
-    k = np.argmax(fft_x)
-    omega_fft = 2 * np.pi * k / (M * T)
-    return omega_fft
-
-def W_estimate(m, M):
-    return (2 * np.pi * m) / (M * T)
-
-def F(w_estimate, standard_deviation):
-
-    sum = 0
-    for n in range(n_0, n_0 + N, 1):
-        sum += X(n*T, standard_deviation) * np.exp(- 1j * w_estimate * n * T)
-
-    F = sum / N
-
-    return F
-
-def Phi_estimate(w_estimate, standard_deviation):
-
-    phi_estimate = np.angle(np.exp(- 1j * w_estimate * n_0 * T) * F(w_estimate, standard_deviation))
-    
-    return phi_estimate
-
-
 
 def Sim_estimation(m, standard_deviation):
 
-    t = np.arange(n_0, n_0 + N, 1) * T
+    t = np.arange(n_0, n_0 + N) * T
     x_values = X(t, standard_deviation)
+
+    w_hat = W_hat_FFT(x_values, m)
     
-    max_fft = Max_FFT(x_values, m)
+    phi_hat = Phi_hat_FFT(x_values, m)
 
-    max_fft = Max_FFT(x_values, m)
+    return w_hat, phi_hat
 
-    w_est = W_estimate(max_fft, m)
-    phi_est = Phi_estimate(w_est, standard_deviation)
+def FindMaxFFTIndex(x, M):
+    fft_x = np.fft.fft(x, M)
+    k = np.argmax(fft_x)
+    return k
 
-    return w_est, phi_est
+def W_hat_FFT(x, M):
+    k = FindMaxFFTIndex(x, M)
+    omega_fft = 2 * np.pi * k / (M * T)
+    return omega_fft
+
+def Phi_hat_FFT(x,M):
+    omega_hat = W_hat_FFT(x,M)
+
+    N = len(x)
+    n = np.arange(n_0, n_0 + N)
+
+    exp_terms = np.exp(-1j * omega_hat * n * T)
+    phi_hat = np.angle(np.mean(x * exp_terms))
+
+    return phi_hat
