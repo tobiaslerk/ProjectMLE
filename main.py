0c92be2d22e971a4e996e4d404d43d81b409eaaf
Simen Borgerud Moe <simenny2506@gmail.com>
2026-04-18 14:57:23 +0200

    Modified the inputs over sim estimation

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import Nelder_Mead_optimization
 t = np.arange(n_0, n_0 + N) * T 
 x_values = estimation.X(t, standard_deviation_list[3])  # Using a specific standard deviation for plotting
 
-#w_estimate, phi_estimate, _ = estimation.Sim_estimation(m_values[5], standard_deviation_list[0])
+w_estimate, phi_estimate, _ = estimation.Sim_estimation(m_values[3], standard_deviation_list[3])
 
-#plotting.PlotEstimationOverlay(t, x_values, w_estimate, phi_estimate)
+plotting.PlotEstimationOverlay(t, x_values, w_estimate, phi_estimate)
 
-#plotting.PlotResults(m_values, standard_deviation_list, num_estimations=1000)
+plotting.PlotResults(m_values, standard_deviation_list, num_estimations=1000)
 
 Nelder_Mead_optimization.task_b(num_estimations=1000)
